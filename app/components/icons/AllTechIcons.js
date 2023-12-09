e9b876c11118ad4128df5b269dd637ca2c479e65
GoorezyEST <goorezyestudios@gmail.com>
2023-12-09 17:29:52 -0300

    Unified all the icons in one component to have easiest time while importing/exporting

---
+++ app/components/icons/AllTechIcons.js
@@ -0,0 +1,47 @@
+import AngularIcon from "./AngularIcon";
+import CSharpIcon from "./CSharpIcon";
+import CssIcon from "./CssIcon";
+import ExpressIcon from "./ExpressIcon";
+import FigmaIcon from "./FigmaIcon";
+import FirebaseIcon from "./FirebaseIcon";
+import FramerMotionIcon from "./FramerMotionIcon";
+import GITIcon from "./GITIcon";
+import GSAPIcon from "./GSAPIcon";
+import HTMLIcon from "./HTMLIcon";
+import JavaScriptIcon from "./JavaScriptIcon";
+import MariaDBIcon from "./MariaDBIcon";
+import MicrosoftSQLServerIcon from "./MicrosoftSQLServerIcon";
+import MongoDBIcon from "./MongoDBIcon";
+import NETCoreIcon from "./NETCoreIcon";
+import NextJSIcon from "./NextJSIcon";
+import NodeJSIcon from "./NodeJSIcon";
+import PhotoshopIcon from "./PhotoshopIcon";
+import PostgreSQLIcon from "./PostgreSQLIcon";
+import ReactJSIcon from "./ReactJSIcon";
+import SassIcon from "./SassIcon";
+import TypeScriptIcon from "./TypeScriptIcon";
+
+export {
+  AngularIcon,
+  CSharpIcon,
+  CssIcon,
+  ExpressIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  FramerMotionIcon,
+  GITIcon,
+  GSAPIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  MariaDBIcon,
+  MicrosoftSQLServerIcon,
+  MongoDBIcon,
+  NETCoreIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  PhotoshopIcon,
+  PostgreSQLIcon,
+  ReactJSIcon,
+  SassIcon,
+  TypeScriptIcon,
+};
