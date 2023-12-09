737241fc0a0a66c0c2170e4df91decea7b45c3c2
GoorezyEST <goorezyestudios@gmail.com>
2023-12-09 17:30:37 -0300

    Function to retrieve icon component of a technology by name

--- functions/Utilities.js
+++ functions/Utilities.js
@@ -3,3 +3,55 @@ export const ResizeImgurImages = (input) => {
 
   return resizedImage;
 };
+
+import {
+  AngularIcon,
+  CSharpIcon,
+  CssIcon,
+  ExpressIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  FramerMotionIcon,
+  GITIcon,
+  GSAPIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  MariaDBIcon,
+  MicrosoftSQLServerIcon,
+  MongoDBIcon,
+  NETCoreIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  PhotoshopIcon,
+  PostgreSQLIcon,
+  ReactJSIcon,
+  SassIcon,
+  TypeScriptIcon,
+} from "@/app/components/icons/AllTechIcons";
+
+const iconsMap = {
+  AngularIcon,
+  CSharpIcon,
+  CssIcon,
+  ExpressIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  FramerMotionIcon,
+  GITIcon,
+  GSAPIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  MariaDBIcon,
+  MicrosoftSQLServerIcon,
+  MongoDBIcon,
+  NETCoreIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  PhotoshopIcon,
+  PostgreSQLIcon,
+  ReactJSIcon,
+  SassIcon,
+  TypeScriptIcon,
+};
+
+export const getTechIcon = (tech) => iconsMap[tech];
