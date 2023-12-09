0771c3c422456eb5ecf7b8a3ed05d4c59b797291
GoorezyEST <goorezyestudios@gmail.com>
2023-12-09 17:28:58 -0300

    Created state to store lang and a function to toggle it

--- contexts/GlobalContext.js
+++ contexts/GlobalContext.js
@@ -5,6 +5,7 @@ const GlobalContext = createContext();
 export function GlobalProvider({ children }) {
   const [isHydrated, setIsHydrated] = useState(true);
   const [smallDevice, setSmallDevice] = useState(false);
+  const [lang, setLang] = useState("en");
 
   useEffect(() => {
     const mediaQueryList = window.matchMedia("(max-width: 700px)");
@@ -12,12 +13,25 @@ export function GlobalProvider({ children }) {
     // Set the initial value
     setSmallDevice(mediaQueryList.matches);
   }, []);
+
+  const toggleLang = () => {
+    if (lang === "es") {
+      setLang("en");
+    }
+    if (lang === "en") {
+      setLang("es");
+    }
+    setLang("es");
+  };
+
   return (
     <GlobalContext.Provider
       value={{
         isHydrated,
         setIsHydrated,
         smallDevice,
+        toggleLang,
+        lang,
       }}
     >
       {children}
