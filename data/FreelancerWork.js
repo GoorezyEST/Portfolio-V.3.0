376e2ced0c56137fadd76c64374c51c7d347459f
GoorezyEST <goorezyestudios@gmail.com>
2023-12-09 17:29:18 -0300

    Added translations

--- data/FreelancerWork.js
+++ data/FreelancerWork.js
@@ -1,4 +1,4 @@
-export const FreelancerWorkList = [
+export const FreelancerWorkListEN = [
   {
     title: "Cardo Audiovisual",
     first_text:
@@ -6,6 +6,19 @@ export const FreelancerWorkList = [
     second_text: "Developed together with a partner under the name Trilipluz.",
     third_text:
       "The main objective was to achieve an aesthetic and visually engaging website to show the work of Cardo Audiovisual. The end result was a gothic aesthetic, with some 3D touches, and extensive use of animations throughout the website.",
+    image_url: "https://i.imgur.com/b1LV2xZ.png",
+    website_url: "https://cardoaudiovisual.com/",
+  },
+];
+
+export const FreelancerWorkListES = [
+  {
+    title: "Cardo Audiovisual",
+    first_text:
+      "Productora de videoclips autogestionada que busca ayudar a los artistas a expresarse audiovisualmente.",
+    second_text: "Desarrollado junto con un socio bajo el nombre de Trilipluz.",
+    third_text:
+      "El objetivo principal era conseguir una web estética y visualmente atractiva para mostrar el trabajo de Cardo Audiovisual. El resultado final fue una estética gótica, con algunos toques 3D, y un amplio uso de animaciones en toda la web.",
     image_url: "https://i.imgur.com/b1LV2xZ.png",
     website_url: "https://cardoaudiovisual.com/",
   },
