00cb2beb8a617150890242afa09d326adf44db9d
GoorezyEST <goorezyestudios@gmail.com>
2024-01-21 17:06:14 -0300

    Fixed website meta title

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,11 @@ const open_Sans = Open_Sans({
 
 export const metadata = {
   metadataBase: new URL("https://franco-espinosa-test.vercel.app/"),
-  title: "Franco Espinosa",
+  title: "Franco Espinosa - Portfolio",
   description:
     "Mi nombre es Franco Espinosa, soy un desarrollador web de Argentina, Mar del Plata. Aprovecho cada día para expandir mis conocimientos y enfrentar desafíos que pongan a prueba mis habilidades.",
   openGraph: {
-    title: "Franco Espinosa",
+    title: "Franco Espinosa - Portfolio",
     description:
       "Mi nombre es Franco Espinosa, soy un desarrollador web de Argentina, Mar del Plata. Aprovecho cada día para expandir mis conocimientos y enfrentar desafíos que pongan a prueba mis habilidades.",
     url: "https://franco-espinosa-test.vercel.app/",
