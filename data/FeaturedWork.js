0fa8035e7e29bbe53067ef93fcf453e0908d8554
GoorezyEST <goorezyestudios@gmail.com>
2024-01-26 17:11:39 -0300

    Moved into project url instead of work

--- data/FeaturedWork.js
+++ data/FeaturedWork.js
@@ -1,17 +1,17 @@
 export const FeaturedWorkList = [
   {
     title: "Snapdle",
-    project_url: "/work/snapdle",
+    project_url: "/proyects/snapdle",
     image_url: "https://i.imgur.com/yoPmRTB.png",
   },
   {
     title: "Get repos",
-    project_url: "/work/get-repos",
+    project_url: "/proyects/get-repos",
     image_url: "https://i.imgur.com/jODuvKN.png",
   },
   {
     title: "Portfolio - V.2",
-    project_url: "work/portfolio-v-2",
+    project_url: "/proyects/portfolio-v-2",
     image_url: "https://i.imgur.com/93KFitz.png",
   },
 ];
