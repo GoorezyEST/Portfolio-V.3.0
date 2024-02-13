d09fd948b0dd90ef9fe14e57d00309e8c62a9e86
GoorezyEST <goorezyestudios@gmail.com>
2024-02-13 17:08:21 -0300

    Test to fix font

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Nunito_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import ThemeProvider from "@/contexts/ThemeProvider";
 
-const open_Sans = Nunito_Sans({
+const font = Nunito_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
 });
@@ -38,7 +38,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={open_Sans.className}>
+      <body className={font.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
